726bc5db28051e65ce95cbeb8d3d4e981d3fbe51
Dominic Claßen <dominic@dominic-classen.de>
2022-10-16 20:39:54 +0200

    fix: remove non working sticker packs

--- web/src/index.js
+++ web/src/index.js
@@ -31,7 +31,7 @@ if (params.has('config')) {
 // This is updated from packs/index.json
 let HOMESERVER_URL = "https://matrix-client.matrix.org"
 
-const makeThumbnailURL = mxc => `${HOMESERVER_URL}/_matrix/media/v3/thumbnail/${mxc.substr(6)}?height=128&width=128&method=scale`
+const makeThumbnailURL = mxc => `${HOMESERVER_URL}/_matrix/media/r0/thumbnail/${mxc.substr(6)}?height=128&width=128&method=scale`
 
 // We need to detect iOS webkit because it has a bug related to scrolling non-fixed divs
 // This is also used to fix scrolling to sections on Element iOS
